4f53dfb9a349bb1723af6d0842a2c884434dd038
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2025-03-31 15:28:05 +0200

    Step 2 - JS : New name instead of pourri Matt

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,18 @@
+// Step 1
+
 const imgAvatar = document.querySelector('#img-avatar');
 
 imgAvatar.addEventListener('click', () => {
     imgAvatar.src = 'image/avatar.svg';
+})
+
+// Step 2
+
+const modifyTextBtn = document.querySelector('.description button');
+
+modifyTextBtn.addEventListener('click', () => {
+    const newName = prompt('Ton nom batard');
+    const currentName = document.querySelector('#firstname');
+    currentName.innerHTML = newName;
+    currentName.style.color = '#ffffff';
 })
