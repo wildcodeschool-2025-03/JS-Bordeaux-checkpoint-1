54543e4dba686c5d103333c206491cb41dce74ad
Morelle <cynthia.morelle.cm@gmail.com>
2025-03-31 12:01:11 +0200

    js step1

---
+++ site/js/portfolio.js
@@ -0,0 +1,20 @@
+/*const currentImg = document.getElementById("avatar");
+
+avatar.addEventListener("click", () => {
+	element.innerHTML = image / avatar - bis.png;
+});*/
+
+const avatar = document.getElementById("avatar");
+avatar.addEventListener("click", () => {
+	avatar.src = "image/avatar-bis.png";
+});
+
+/*function onButtonClick(event) {
+  counter++;
+
+  const button = event.target;
+  button.innerHTML = "Nombre d'articles : " + counter;
+}
+
+const button = document.querySelector("button")
+button.addEventListener("click", onButtonClick);*/
