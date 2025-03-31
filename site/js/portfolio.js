74c3c56537a80aec65d833f53c096d046f18e284
Valentin Ferreira <ferreira.pro37@gmail.com>
2025-03-31 11:53:14 +0200

    js checkpoint 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,3 +4,13 @@ avatar.onclick = function () {
     avatar.src= "image/avatar.svg"
 }
 
+const boutonNom = document.querySelector('.boutonprenom')
+const firstName = document.querySelector('#firstname')
+const background = document.querySelector('.pink-bg')
+
+boutonNom.onclick = function () {
+    background.style.backgroundColor = prompt("Enter the color")
+    firstName.textContent = prompt("Enter your name:")
+    firstName.style.color = 'white'
+    
+}
