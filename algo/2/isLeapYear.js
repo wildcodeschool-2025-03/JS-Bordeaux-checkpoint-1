88445291744e3287c5902fe70af2b1cfc86487e2
Morelle <cynthia.morelle.cm@gmail.com>
2025-03-31 09:59:37 +0200

    update algo2 part1

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -8,7 +8,12 @@ Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utilise
 */
 
 function isLeapYear(year) {
-  // Your code here !
+	if (year % 400 === 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
+// Your code here !
 
 module.exports = isLeapYear;
