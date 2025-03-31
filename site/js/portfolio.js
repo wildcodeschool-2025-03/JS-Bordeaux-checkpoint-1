cca80e3c9a4fbb7136ad515f5e1c07190034487f
yakup <yakup.senel33@gmail.com>
2025-03-31 11:56:53 +0200

    finish JS step 2 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,12 @@ const avatar = document.querySelector('.img-avatar');
 avatar.addEventListener('click', () => {
     avatar.src = "image/avatar.svg";
 });
+
+const buttonTextColor = document.querySelector('.modifyTextColor');
+const firstName = document.querySelector('#firstname');
+
+buttonTextColor.addEventListener('click', () => {
+    const newFirstName =prompt('Enter your name');
+    firstName.textContent = newFirstName;
+    firstName.style.color="black";
+})
