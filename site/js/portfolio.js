efabe4a7687abe1f8421d5f7ca89fac652f69a34
Morelle <cynthia.morelle.cm@gmail.com>
2025-03-31 12:56:53 +0200

    tried js step2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,17 +4,19 @@ avatar.addEventListener("click", () => {
 	element.innerHTML = image / avatar - bis.png;
 });*/
 
+/*first step*/
 const avatar = document.getElementById("avatar");
 avatar.addEventListener("click", () => {
 	avatar.src = "image/avatar-bis.png";
 });
 
-/*function onButtonClick(event) {
-  counter++;
-
-  const button = event.target;
-  button.innerHTML = "Nombre d'articles : " + counter;
+/*second step*/
+const button = document.getElementById("modify");
+button.addEventListener("click", onButtonClick);
+function onButtonClick(event) {
+	const enterYourName = prompt("enter your name");
+	if (enterYourName !== null && enterYourName !== "") {
+		const firstName = document.getElementById("firstname");
+		firstName.innerHTML = enterYourName;
+	}
 }
-
-const button = document.querySelector("button")
-button.addEventListener("click", onButtonClick);*/
