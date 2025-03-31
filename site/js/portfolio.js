430f0fef5d12167d0585685a47b0ee651e9f6bf4
Valentin Ferreira <ferreira.pro37@gmail.com>
2025-03-31 12:08:05 +0200

    step 4 js

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -7,10 +7,15 @@ avatar.onclick = function () {
 const boutonNom = document.querySelector('.boutonprenom')
 const firstName = document.querySelector('#firstname')
 const background = document.querySelector('.pink-bg')
+const pinkText = document.querySelectorAll('.pink-text')
 
 boutonNom.onclick = function () {
-    background.style.backgroundColor = prompt("Enter the color")
+    const theColor = prompt("Enter the color")
+    background.style.backgroundColor = theColor
     firstName.textContent = prompt("Enter your name:")
     firstName.style.color = 'white'
-    
+    pinkText.forEach(function(element){
+        element.style.color = theColor;
+    });
+
 }
