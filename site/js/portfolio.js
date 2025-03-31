aec6288c57c47244f841d377fdb53f404a91cacb
Bene SAYER <bentouster@gmail.com>
2025-03-31 13:33:53 +0200

    modification portfolio

---
+++ site/js/portfolio.js
@@ -0,0 +1,30 @@
+
+//* change avatar//
+const avatarImg = document.querySelector('.img-avatar');
+avatarImg.src = "algo1/JS-Bordeaux-checkpoint-1/site/image/avatar.svg";
+
+
+//* change Username//
+const userName = prompt("What's your name ?");
+
+const firstName = document.querySelector('.firstname');
+
+firstName.innerHTML = `I am , ${userName}.`
+
+//* change pink background color// 
+const colorBackground = prompt("Type your color?");
+
+const color = document.querySelector('.background-color');
+background - color.innerCSS = '  #750ff7';
+
+//* change all pink elements  // 
+const pinkElements = prompt("Type your color?");
+
+const color = document.querySelectorAll('.var(--light)');
+background - color.innerCSS = '  #750ff7';
+
+
+
+
+
+
